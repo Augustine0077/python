e483d70284ec8408d2c77c8c1b4cf0dc5a071c39
Augustine Shaji <135913304+Augustine0077@users.noreply.github.com>
2025-05-21 23:46:15 +0530

    Add files via upload

---
+++ gratest_of_3_numbers.py
@@ -0,0 +1,9 @@
+num1=2
+num2=5
+num3=10
+if num1>num2 and num1>num3:
+    print(f"{num1} is gratest")
+elif num2 > num1 and num2 >num3:
+    print(f"{num2} is gratest")
+else:
+    print(f"{num3}is gratest")
