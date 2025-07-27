111d97db258e1a37197f746301f6af1adfee63dd
Augustine0077 <135913304+Augustine0077@users.noreply.github.com>
2025-07-27 22:19:01 +0530

    weight convertion

--- calculatro.py
+++ calculatro.py
@@ -1,28 +1,25 @@
-num1=int(input("Enter first number: "))
+num1 = int(input("Enter first number: "))
 
 print("Choose an operator ( +,- , *,/)")
-
 print("for '+' enter = 1\n for '-' enter = 2\n for '*' enter = 3\n for '/' enter = 4\n")
 
-operator=input("Enter operator: ")
-
-num2=int(input("Enter second number: "))
-sum = 0
+operator = input("Enter operator: ")
+num2 = int(input("Enter second number: "))
 
 if operator == "1":
-    sum = num1+num2
-if operator == "2":
-    sum = num1-num2
-if operator == "3":
-    sum = num1*num2
-if operator == "4":
+    result = num1 + num2
+    print(f"answer  = {result}")
+elif operator == "2":
+    result = num1 - num2
+    print(f"answer  = {result}")
+elif operator == "3":
+    result = num1 * num2
+    print(f"answer  = {result}")
+elif operator == "4":
     if num2 != 0:
-        sum = num1/num2
+        result = num1 / num2
+        print(f"answer  = {result}")
     else:
         print("Cannot divide by zero")
-        exit()
-
-
-print(f"answer  = {sum}")
 else:
     print("Invalid operator")
