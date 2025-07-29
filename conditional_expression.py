cc81333d6d98c88d63d3143c8fc587cd05a9bae2
Augustine0077 <135913304+Augustine0077@users.noreply.github.com>
2025-07-29 22:00:25 +0530

    the conditional operator in python

---
+++ conditional_expression.py
@@ -0,0 +1,19 @@
+# num = 5
+# result = "Even" if num % 2 ==0 else "Odd"
+# print(result)
+
+num_1 =5
+num_2=10
+greater = num_1 if num_1 > num_2 else num_2
+print(greater)
+
+#     value_if_true if condition else value_if_false
+
+# A conditional expression in Python, also known as the ternary operator, 
+# provides a concise way to write an if-else statement on a single line.
+#  It evaluates a condition and returns one value if the condition is true, 
+# and another value if the condition is false.
+
+# This is the conditional expression in python just remeber the formula
+
+# return x if condition else y
