bc2dda5dd68c2a701808a9f646feb6a25d682360
Augustine0077 <135913304+Augustine0077@users.noreply.github.com>
2025-08-01 23:07:06 +0530

    compund intrest calculator

--- while_loop.py
+++ while_loop.py
@@ -1,7 +1,8 @@
 #exicute a stament while a condition is true
 name = input("Enter your name :")
-#while name == "":
-if name == "":
+while name == "":
+#if name == "":
+# if if stament is used one time only exicution will happen 
     print("You entered a blank name. Please try again.")
     name = input("Enter your name :") 
 print(f"Hello, {name}!")  # Output: Hello, [name]   
