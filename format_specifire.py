326a3bf2338c53bb2a70e7ebf0eb9ddc1fc3a182
Augustine0077 <135913304+Augustine0077@users.noreply.github.com>
2025-08-01 22:37:47 +0530

    format specifier

---
+++ format_specifire.py
@@ -0,0 +1,10 @@
+# format specifier is nothing but if we want to do how many numbers need to print #A format specifier in Python is a way to control how values are displayed when using formatted strings.
+#It lets you specify things like the number of decimal places, alignment, width, and type (integer, float, string, etc.).
+
+#after the point(.)
+num_1=1.22345678
+num2=123.45
+num3=1.234567890
+print(f"num1 is {num_1:.1f}")
+print(f"num2 is {num2:.5f}")
+print(f"num3 is {num3:.5f}")
