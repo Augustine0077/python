36bc249c1125058fe3db6a111d9a0d12f86bb074
Augustine Shaji <augustineshaji007@gmail.com>
2025-09-10 23:52:17 +0530

    first n natural numbers in python

--- first_n_natural_numbers.py
+++ first_n_natural_numbers.py
@@ -1,5 +1,14 @@
+# number = int(input("Enter a number: "))
+# total = 0
+# i = 1
+# while i <= number:
+#     total += i
+#     i += 1
+# print(total)
+
 number = int(input("Enter a number: "))
-sum = 1
-while sum == number:
-    sum+=1
-print(sum)
+total = 1
+while total <= number:
+    print(total,end = " ")
+    total += 1
+    
