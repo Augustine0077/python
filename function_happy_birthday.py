3d5e5bf42a7f52cd65d41bbbb23310bb03f1ca2e
Augustine Shaji <augustineshaji007@gmail.com>
2025-09-15 14:02:39 +0530

    happy birthday usin functions

---
+++ function_happy_birthday.py
@@ -0,0 +1,8 @@
+def happy_birthday():
+    print("Happy Birthday to You!")
+    print("Happy Birthday to You!")
+    print("Happy Birthday Dear augustine!")
+    print("Happy Birthday to You!")
+
+happy_birthday()
+happy_birthday()
