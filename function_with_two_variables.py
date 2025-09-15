fe2487df0b5127936017d60cac1ea5ec6756774a
Augustine Shaji <augustineshaji007@gmail.com>
2025-09-15 14:08:02 +0530

    happy birthday usin function 2 variable

---
+++ function_with_two_variables.py
@@ -0,0 +1,8 @@
+def happy_birthday(name,age):
+    print("Happy Birthday to You!")
+    print("Happy Birthday to You!")
+    print(f"Happy Birthday Dear {name}!")
+    print("Happy Birthday to You!")
+    print(f"You are now {age} years old!")
+
+print(happy_birthday("augustine", 20))
