2e5151aa0ca83419579b6b85e0f210b03f2e055a
Augustine Shaji <augustineshaji007@gmail.com>
2025-09-15 14:04:59 +0530

    happy birthday usin function variable

---
+++ function_with_variable.py
@@ -0,0 +1,8 @@
+def happy_birthady(name):
+    print("Happy Birthday to You!")
+    print("Happy Birthday to You!")
+    print(f"Happy Birthday Dear {name}!")
+    print("Happy Birthday to You!")
+
+name = input("Enter the name of the birthday person: ")
+happy_birthady(name)
