4aff0b1cebf084805157439807aad7656a958dd7
Augustine Shaji <augustineshaji007@gmail.com>
2025-10-02 10:35:33 +0530

    arbitary using program

---
+++ arbitary_args.py
@@ -0,0 +1,6 @@
+def add(*args):
+    total= 0
+    for x in args:
+        total+=x
+    return total
+print(add(1 , 2, 4))
--- voting_system.py
+++ voting_system.py
@@ -1,2 +1,15 @@
-age=int(input("Enter your age: "))
-print("can vote") if age >=18 else print("cannot vote")
+# # age=int(input("Enter your age: "))
+# # print("can vote") if age >=18 else print("cannot vote")
+# def net_price(orginal_price,discount=-5,tax=50):
+#     return(orginal_price + discount+tax)
+# print( net_price(500))
+
+import time
+
+def timer(start,end):
+    for x in range(start,end+1):
+        print(x)
+        time.sleep(1)
+    print("Done !",end =" " )
+
+timer(0,5)
