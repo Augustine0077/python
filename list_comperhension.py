81d80ba18dfd6d984f3c853372bc870542ed20e8
Augustine Shaji <augustineshaji007@gmail.com>
2025-10-03 22:59:30 +0530

    list comperhension

--- list_comperhension.py
+++ list_comperhension.py
@@ -2,5 +2,9 @@
 # for i in range(1,11):
 #     double.append(i*2)
 # print(double)
-double =[x*2 for x in range(1,11)]
-print(double)
+# double =[x*2 for x in range(1,11)]
+# print(double)
+
+fruits=["apple","banana","orange"]
+fruits=[x.upper() for x in fruits]
+print(fruits)
