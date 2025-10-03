5cdc21f0c470ff841f8d11b64578510c81d507c6
Augustine Shaji <augustineshaji007@gmail.com>
2025-10-03 22:53:06 +0530

    list comperhension

--- list_comperhension.py
+++ list_comperhension.py
@@ -1,4 +1,6 @@
-double=[]
-for i in range(1,11):
-    double.append(i*2)
+# double=[]
+# for i in range(1,11):
+#     double.append(i*2)
+# print(double)
+double =[x*2 for x in range(1,11)]
 print(double)
