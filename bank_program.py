c0bbf75af298eed732f24e78414e2a35f56dd4fd
Augustine Shaji <augustineshaji007@gmail.com>
2025-10-04 23:50:29 +0530

    bank program

--- bank_program.py
+++ bank_program.py
@@ -1,49 +1,37 @@
 def show_balance():
     print(f"Your current balance is: ${balance:.2f}")
-
 def deposit():
-    amount = float(input("Enter amount to deposit: "))
-    if amount < 0:
-        print("Deposit amount must be positive.")
+    amount = float(input("Enter amount to deposit: $"))
+
+    if balance < 0:
+        print("The balance cannot be less than 0")
         return 0
     else:
         return amount
-
 def withdraw():
-    global balance
-    amount = float(input("Enter amount to withdraw: "))
-    if amount < 0:
-        print("Withdraw amount must be positive.")
-    elif amount > balance:
-        print("Insufficient funds.")
-    else:
-        balance -= amount
-        print(f"${amount:.2f} withdrawn.")
+    amount = float(input("Enter the amout to withdrw"))
+
+    if amount > balance:
+        print("Insufficent fund")
 
 balance = 0
 is_running = True
-
 while is_running:
-    print("welcom to the bank program")
-    print("1. Show Balance")
-    print("2. Deposit") 
+    print("welcome to the bank program")
+    print("1. Show balance")
+    print("2. Deposit")
     print("3. Withdraw")
     print("4. Exit")
 
-    try:
-        Choice = int(input("Enter your choice (1-4): "))
-    except ValueError:
-        print("Please enter a valid number.")
-        continue
-
-    if Choice == 1:
+    choice = input("Choose an option between 1-4:   ")
+    if choice == '1':
         show_balance()
-    elif Choice == 2:
-        balance += deposit()
-    elif Choice == 3:   
+    elif choice == '2':
+       balance += deposit()
+    elif choice == '3':
         withdraw()
-    elif Choice == 4:   
+    elif choice == '4':
         is_running = False
     else:
-        print("That is not a valid choice. Please choose a number between 1 and 4.")
+        print("Invalid option, please choose a number between 1-4.")
 print("Thank you for using the bank program. Goodbye!")
