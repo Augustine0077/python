9f94f1c7c321bc2059ca2f3c2fa413ee30cb5925
Augustine Shaji <augustineshaji007@gmail.com>
2025-10-05 13:05:00 +0530

    bank program

--- bank_program.py
+++ bank_program.py
@@ -1,26 +1,82 @@
+# balance = 0.0
+
+# def show_balance():
+#     print(f"Your current balance is: ${balance:.2f}")
+
+# def deposit():
+#     amount = float(input("Enter amount to deposit: $"))
+
+#     if amount < 0:
+#         print("Deposit amount must be positive.")
+#         return 0
+#     else:
+#         return amount
+
+# def withdraw():
+#     # global balance
+#     amount = float(input("Enter the amount to withdraw: $"))
+
+#     if amount > balance:
+#         print("Insufficient funds.")
+#         return 0
+#     elif amount < 0:
+#         print("Amount must be greater than 0.")
+#         return 0
+#     else:
+#         return amount
+        
+
+# def main():
+#     global balance
+#     is_running = True
+#     while is_running:
+#         print("welcome to the bank program")
+#         print("1. Show balance")
+#         print("2. Deposit")
+#         print("3. Withdraw")
+#         print("4. Exit")
+
+#         choice = input("Choose an option between 1-4: ")
+#         if choice == '1':
+#             show_balance()
+#         elif choice == '2':
+#             balance += deposit()
+#         elif choice == '3':
+#             balance -= withdraw()
+#         elif choice == '4':
+#             is_running = False
+#         else:
+#             print("Invalid option, please choose a number between 1-4.")
+#     print("Thank you for using the bank program. Goodbye!")
+
+# if __name__ == "__main__":
+#     main()
+
+
+
+
+
 balance = 0.0
 
 def show_balance():
     print(f"Your current balance is: ${balance:.2f}")
 
 def deposit():
-    amount = float(input("Enter amount to deposit: $"))
-
-    if amount < 0:
-        print("Deposit amount must be positive.")
+    amount = float(input("Enter the amount: "))
+    if amount <= 0:
+        print("The amount should be positive")
         return 0
     else:
         return amount
 
 def withdraw():
     global balance
-    amount = float(input("Enter the amount to withdraw: $"))
-
+    amount = float(input("Enter the withdraw amount: "))
     if amount > balance:
-        print("Insufficient funds.")
+        print("Insufficient funds")
         return 0
     elif amount < 0:
-        print("Amount must be greater than 0.")
+        print("Amount should be greater than 0")
         return 0
     else:
         return amount
@@ -29,24 +85,25 @@ def main():
     global balance
     is_running = True
     while is_running:
-        print("welcome to the bank program")
+        print("Welcome to the bank program")
         print("1. Show balance")
         print("2. Deposit")
         print("3. Withdraw")
         print("4. Exit")
 
-        choice = input("Choose an option between 1-4: ")
-        if choice == '1':
+        choice = int(input("Choose an option between 1-4: "))
+
+        if choice == 1:
             show_balance()
-        elif choice == '2':
+        elif choice == 2:
             balance += deposit()
-        elif choice == '3':
+        elif choice == 3:
             balance -= withdraw()
-        elif choice == '4':
+        elif choice == 4:
             is_running = False
         else:
-            print("Invalid option, please choose a number between 1-4.")
-    print("Thank you for using the bank program. Goodbye!")
+            print("Invalid operation")
+    print("Thanks for using this bank")
 
 if __name__ == "__main__":
     main()
