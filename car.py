56ffebda89ca8952fb1689f2ec6f5fb5ecb9a531
Augustine Shaji <augustineshaji007@gmail.com>
2025-10-08 14:11:23 +0530

    taking the object from aother file using import

---
+++ car.py
@@ -0,0 +1,6 @@
+class Car:
+    def __init__(self, make, model, year,for_sale):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.for_sale = for_sale
--- object_oriented.py
+++ object_oriented.py
@@ -1,10 +1,4 @@
-class Car:
-    def __init__(self, make, model, year,for_sale):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.for_sale = for_sale
-
+from car import Car
 Car1 = Car("Toyota", "Corolla", 2020, True)
 print(Car1.make) 
 print(Car1.model)
