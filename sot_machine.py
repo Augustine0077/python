c33eab72f62dd497f3627bb1a3bea900b6b5653b
Augustine Shaji <augustineshaji007@gmail.com>
2025-10-08 10:22:36 +0530

    sot machine program

--- sot_machine.py
+++ sot_machine.py
@@ -1,38 +1,26 @@
 def spin_row():
     pass
+
 def print_row():
     pass
+
 def get_payout():
     pass
 
 def main():
-    balance = 100.0
+    balance = 100
 
-
+    print("******************************")
     print("Welcome to the Slot Machine!")
-    print("*************")
-    print("Symbols: A, B, C, D")
-    print("**************")
-    while  balance > 0:
-        print(f"Your balance is: ${balance:.2f}")
-        bet = input("Enter your bet amount")
+    print("1--2--3--4--5")
+    print("******************************")
 
-        if not bet.isdegit():
-
-
+    while balance > 0:
+        print(f"your current balance is {balance}")
+        bet = input("Enter a bet amount : ")
+        if not bet.isdigit():
+            print("Please enter a valid number")
+            continue
 
 if __name__ == "__main__":
     main()
-    choice = input("Choose an option between 1-4:   ")
-    if choice == '1':
-        show_balance()
-    elif choice == '2':
-    balance += deposit()
-    elif choice == '3':
-        balance -= withdraw()
-    elif choice == '4':
-        is_running = False
-    else:
-        print("Invalid option, please choose a number between 1-4.")
-print("Thank you for using the bank program. Goodbye!")
-
