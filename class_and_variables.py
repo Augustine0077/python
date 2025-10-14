7b33e2f03397fd8c922c4c61af897e027685546c
Augustine Shaji <augustineshaji007@gmail.com>
2025-10-14 20:52:39 +0530

    class and variables in python

--- class_and_variables.py
+++ class_and_variables.py
@@ -10,7 +10,8 @@ class Students:
 student1 = Students("Augustine",22)
 student2 = Students("John",23)
 student3 = Students("Doe",24)
-print(student1.name)
-print(student2.age)
-print(student1.Year_passout)
-print(Students.number_of_students)
+print(f" my name is {student1.name}")
+print(f" my age is {student2.age}")
+print(f" I will graduate in {student1.Year_passout}")
+
+print(f" I am graduating in {Students.Year_passout} and in my class there are {Students.number_of_students} students")
