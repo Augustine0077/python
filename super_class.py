5de1957701cc1f6fb1a3c17be4ea418d286a1b35
Augustine Shaji <augustineshaji007@gmail.com>
2025-10-18 22:00:15 +0530

    super class practicing

--- super_class.py
+++ super_class.py
@@ -4,17 +4,17 @@ class Shape:
         self.is_filled = is_filled
 class Circle:
     def __init__(self,colour,radius,is_filled):
-        self.colour = colour 
+        super().__init__(colour,is_filled)
         self.radius = radius
-        self.is_filled = is_filled
+        
 class Square:
     def __init__(self,colour,width,is_filled):
-        self.colour = colour 
+        super().__init__(colour,is_filled)
         self.width = width
-        self.is_filled = is_filled
+        
 class Triangle:
     def __init__(self,colour,width,height,is_filled):
-        self.colour = colour 
+        super().__init__(colour,is_filled)
         self.width = width
         self.height = height
-        self.is_filled = is_filled
+       
