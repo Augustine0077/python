8e1bfda51fb24a0a33b4cae75e322823ad716297
Augustine Shaji <augustineshaji007@gmail.com>
2025-10-25 23:42:22 +0530

    list comperhension

--- comperhensio.py
+++ comperhensio.py
@@ -7,7 +7,7 @@ for i in mark:
         passed.append(i)
 print("Passed marks are:", passed)
 
-# # using comprehension
-# mark = [50,60,30,20,10,70,80,90,100]
-# passed = [i for i in mark if i>=60]
-# print("Passed marks are:", passed)
+# using comprehension
+mark = [50,60,30,20,10,70,80,90,100]
+passed = [i for i in mark if i>=60]
+print("Passed marks are:", passed)
