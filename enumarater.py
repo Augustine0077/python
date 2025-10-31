a642f48814f982a9c1f31a0f5e820238e8d123b6
Augustine Shaji <augustineshaji007@gmail.com>
2025-10-31 19:22:35 +0530

    enumartor function

--- enumarater.py
+++ enumarater.py
@@ -1,6 +1,9 @@
 backbag = ['box','book','scale','pencil','pen']
 
-for i in range(len(backbag)):
+# for i in range(len(backbag)):
 
-    print(i, backbag[i])
-    
+#     print(i, backbag[i])
+
+for i, value in enumerate(backbag):
+
+    print(i, value)
