254bf2c119760aa009b1f522d4005b11b9f34cf8
Augustine Shaji <augustineshaji007@gmail.com>
2025-11-01 18:33:24 +0530

    lambda function

--- map_function.py
+++ map_function.py
@@ -1,8 +1,18 @@
-def c_to_f(temp):
-    return (temp * 9/5) + 32
+# def c_to_f(temp):
+#     return (temp * 9/5) + 32
 
-celsius = [0.1, 10.0, 20.0, 30.0, 40.0, 50.0]
+# celsius = [0.1, 10.0, 20.0, 30.0, 40.0, 50.0]
 
-farienheit = map(c_to_f,celsius)
-for i in farienheit:
+# farienheit = map(c_to_f,celsius)
+# for i in farienheit:
+#     print(i)
+
+
+
+
+list = [1,2,3,4,5,6,7,8,9]
+
+lam = lambda x: x* 2
+
+for i in map(lam,list):
     print(i)
