fa5491511e45f919f01a4976b036562b88723597
Augustine Shaji <augustineshaji007@gmail.com>
2025-11-02 20:50:30 +0530

    generators

--- iter_tools.py
+++ iter_tools.py
@@ -1,10 +1,24 @@
-fruits = ['apple','banana','orange','grape','mango']
+# fruits = ['apple','banana','orange','grape','mango']
 
-i = iter(fruits)
+# i = iter(fruits)
 
-print(next(i))
-print(next(i))
-print(next(i))
-print(next(i))
-print(next(i))
-print(next(i))  # This will raise StopIteration error since there are no more items to
+# print(next(i))
+# print(next(i))
+# print(next(i))
+# print(next(i))
+# print(next(i))
+# print(next(i))  # This will raise StopIteration error since there are no more items to
+
+
+
+def calculate():
+    number  =  2
+    while True:
+        yield number
+        num = number * number
+
+
+gen = calculate()
+print(next(gen))
+print(next(gen))
+print(next(gen))
